8f0a9c71bfb334415119273ebcc2527ef74cc654
Keyrock2 <mbrock1047@gmail.com>
2025-12-09 13:30:29 +0900

    "과제제출"

--- logger.h
+++ logger.h
@@ -1,30 +1,61 @@
-
+#pragma once
 #include <iostream>
 #include <fstream>
 #include <mutex>
 #include <thread>
 #include <string>
+#include <memory>
 
-class Logger {
+class Logger
+{
 private:
     static std::unique_ptr<Logger> instance;
     static std::mutex init_mtx;
     static std::mutex write_mtx;
     std::ofstream logFile;
 
+    Logger(const std::string &filename)
+    {
+        logFile.open(filename, std::ios::trunc);
+        if (logFile.is_open())
+        {
+            logFile << "[Init] Logger started." << std::endl;
+        }
+    }
 
 public:
-    static Logger* getInstance(const std::string& filename = "Test/output2.txt") {
-        return nullptr;
+    Logger(const Logger &) = delete;
+    Logger &operator=(const Logger &) = delete;
+
+    ~Logger()
+    {
+        if (logFile.is_open())
+        {
+            logFile << "[Shutdown] Logger closed." << std::endl;
+            logFile.close();
+        }
     }
 
-    void log(const std::string& message) {
-        /* TODO */
+    static Logger *getInstance(const std::string &filename = "Test/output2.txt")
+    {
+        std::lock_guard<std::mutex> lock(init_mtx);
+        if (instance == nullptr)
+        {
+            instance = std::unique_ptr<Logger>(new Logger(filename));
+        }
+        return instance.get();
     }
 
+    void log(const std::string &message)
+    {
+        std::lock_guard<std::mutex> lock(write_mtx);
+        if (logFile.is_open())
+        {
+            logFile << message << std::endl;
+        }
+    }
 };
 
 std::unique_ptr<Logger> Logger::instance;
 std::mutex Logger::init_mtx;
 std::mutex Logger::write_mtx;
-
--- main.cpp
+++ main.cpp
@@ -2,44 +2,74 @@
 #include <memory>
 #include <vector>
 #include "character.h"
+#include "logger.h"
 
 using namespace std;
 
-void equip_test1(shared_ptr<Character> c1) {
-    c1 = make_shared<Armor>(c1);
-    c1 = make_shared<Boots>(c1);
-    c1 = make_shared<Sword>(c1);
-    cout << c1->getDescription()
-        << " | Attack: " << c1->getAttack()
-        << " | Speed: " << c1->getSpeed()
-        << " | Defense: " << c1->getDefense() << endl;
+void equip_test1(shared_ptr<Character> c1)
+{
+    try
+    {
+        c1 = make_shared<Armor>(c1);
+        c1 = make_shared<Boots>(c1);
+        c1 = make_shared<Sword>(c1);
+        cout << c1->getDescription()
+             << " | Attack: " << c1->getAttack()
+             << " | Speed: " << c1->getSpeed()
+             << " | Defense: " << c1->getDefense() << endl;
+    }
+    catch (const std::exception &e)
+    {
+        cout << "Invalid equip: " << e.what() << endl;
+        Logger::getInstance()->log("[Fail to Equip] ");
+    }
 }
 
-void equip_test2(shared_ptr<Character> c1) {
-    c1 = make_shared<Armor>(c1);
-    c1 = make_shared<Bow>(c1);
-    cout << c1->getDescription()
-        << " | Attack: " << c1->getAttack()
-        << " | Speed: " << c1->getSpeed()
-        << " | Defense: " << c1->getDefense() << endl;
+void equip_test2(shared_ptr<Character> c1)
+{
+    try
+    {
+        c1 = make_shared<Armor>(c1);
+        c1 = make_shared<Bow>(c1);
+        cout << c1->getDescription()
+             << " | Attack: " << c1->getAttack()
+             << " | Speed: " << c1->getSpeed()
+             << " | Defense: " << c1->getDefense() << endl;
+    }
+    catch (const std::exception &e)
+    {
+        cout << "Invalid equip: " << e.what() << endl;
+        Logger::getInstance()->log("[Fail to Equip] ");
+    }
 }
 
-void equip_test3(shared_ptr<Character> c1) {
-    c1 = make_shared<Boots>(c1);
-    c1 = make_shared<Staff>(c1);
-    cout << c1->getDescription()
-        << " | Attack: " << c1->getAttack()
-        << " | Speed: " << c1->getSpeed()
-        << " | Defense: " << c1->getDefense() << endl;
+void equip_test3(shared_ptr<Character> c1)
+{
+    try
+    {
+        c1 = make_shared<Boots>(c1);
+        c1 = make_shared<Staff>(c1);
+        cout << c1->getDescription()
+             << " | Attack: " << c1->getAttack()
+             << " | Speed: " << c1->getSpeed()
+             << " | Defense: " << c1->getDefense() << endl;
+    }
+    catch (const std::exception &e)
+    {
+        cout << "Invalid equip: " << e.what() << endl;
+        Logger::getInstance()->log("[Fail to Equip] ");
+    }
 }
 
-void doTest() {
+void doTest()
+{
     vector<shared_ptr<Character>> players;
     players.push_back(make_shared<Knight>());
     players.push_back(make_shared<Wizard>());
     players.push_back(make_shared<Archer>());
 
-    for(auto player: players){
+    for (auto player : players)
+    {
         cout << "-------------------------------------------------------------" << endl;
         equip_test1(player);
         equip_test2(player);
@@ -47,18 +77,15 @@ void doTest() {
     }
     cout << "-------------------------------------------------------------" << endl;
 
+    for (size_t i = 0; i < players.size(); ++i)
+    {
+        players[i].reset();
+    }
 }
 
-struct Student{
-    string name;
-    Student(string name): name(name) {};
-    Student() = delete;
-    ~Student() { cout << "Delete Student " << name << endl; }
-};
-
-
-
-int main(int argc, const char *argv[]) {
+int main(int argc, const char *argv[])
+{
+    Logger::getInstance();
     doTest();
     return 0;
 }
